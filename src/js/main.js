a7f3f042b38c5fc3caf87f04d4d246ed424d1c19
Вячеслав <spychel1@yandex.ru>
2023-03-11 18:03:50 +0800

    Добавил задержку запроса 200ms чтобы не спамить API

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,30 @@ import { errorBloCkHandler } from "./SearchWidget/errorBlockHandler.js";
 const input = document.getElementsByName("search-field")[0];
 const searchButton = document.getElementsByName('search-button')[0];
 
+let timeoutSearchId = null;
+
 input.addEventListener('keyup', onKeyUpEnter);
 searchButton.addEventListener('click', onClickSearchButton);
 
 function onKeyUpEnter(e) {
     let query = e.target.value;
     if (e.key === "Enter") {
+        if (timeoutSearchId)
+            clearTimeout(timeoutSearchId);
         search(query);
     }
+
 }
 
 function onClickSearchButton() {
     let query = input.value;
+    if (timeoutSearchId)
+        clearTimeout(timeoutSearchId);
     search(query);
 }
 
 function search(query) {
     if (errorBloCkHandler(query)) {
-        resultBlockHandler(query);
+        timeoutSearchId = setTimeout(() => resultBlockHandler(query), 200);
     }
 }
